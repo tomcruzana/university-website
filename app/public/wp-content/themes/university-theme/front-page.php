58617c333e9014fbb8ada69465f65be6e247d619
tomcruzana <tomcruzana@ymail.com>
2024-01-25 14:54:19 -0500

    UW-4 add event post type

--- app/public/wp-content/themes/university-theme/front-page.php
+++ app/public/wp-content/themes/university-theme/front-page.php
@@ -19,30 +19,35 @@ get_header();
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
+            <?php
+            $event_posts = new WP_Query(
+                array('posts_per_page' => 2, 'post_type' => 'event')
+            );
+
+            while ($event_posts->have_posts()) {
+                $event_posts->the_post();
+            }
+            ?>
+
             <div class="event-summary">
                 <a class="event-summary__date t-center" href="#">
                     <span class="event-summary__month">Mar</span>
                     <span class="event-summary__day">25</span>
                 </a>
                 <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
-                    <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a
-                            href="#" class="nu gray">Learn more</a></p>
-                </div>
-            </div>
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Apr</span>
-                    <span class="event-summary__day">02</span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
-                    <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#"
-                            class="nu gray">Learn more</a></p>
+                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>">
+                            <?php the_title() ?>
+                        </a></h5>
+                    <p>
+                        <?php echo wp_trim_words(get_the_content(), 18) ?><a href="<?php the_permalink() ?>"
+                            class="nu gray">Learn More</a>
+                    </p>
                 </div>
             </div>
 
-            <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+            <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link("event"); ?>"
+                    class="btn btn--blue">View All
+                    Events</a></p>
         </div>
     </div>
     <div class="full-width-split__two">
@@ -74,8 +79,15 @@ get_header();
                         </a>
                     </h5>
                     <p>
-                        <?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>"
-                            class="nu gray">Read more</a>
+                        <?php
+                        // check if there's an excerpt configured on the wp admin
+                        // else trim words
+                        if (has_excerpt()) {
+                            the_excerpt();
+                        } else {
+                            echo wp_trim_words(get_the_content(), 18);
+                        } ?>
+                        <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a>
                     </p>
                 </div>
             </div>
