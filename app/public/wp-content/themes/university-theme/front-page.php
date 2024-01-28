531cdb775335701060f54107e776c7c03142d8f4
tomcruzana <tomcruzana@ymail.com>
2024-01-28 05:25:31 -0500

    implemented custom date field query sorting

--- app/public/wp-content/themes/university-theme/front-page.php
+++ app/public/wp-content/themes/university-theme/front-page.php
@@ -20,8 +20,22 @@ get_header();
             <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
             <?php
+            // order custom date fields
+            $today = date('Ymd');
             $event_posts = new WP_Query(
-                array('posts_per_page' => 2, 'post_type' => 'event')
+                array(
+                    'posts_per_page' => -1,
+                    'post_type' => 'event',
+                    'meta_key' => 'event_date',
+                    'orderby' => 'meta_value_num',
+                    'order' => 'ASC',
+                    'meta_query' => array(
+                        'key' => 'event_date',
+                        'compare' => '>=',
+                        'value' => $today,
+                        'type' => 'numeric'
+                    ),
+                )
             );
 
             while ($event_posts->have_posts()) {
