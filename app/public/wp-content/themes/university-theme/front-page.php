e193e0d25422bda1b3edde24a28eb4f1ff9735a5
tomcruzana <tomcruzana@ymail.com>
2024-01-28 05:10:41 -0500

    added custom fields using ACF plugin

--- app/public/wp-content/themes/university-theme/front-page.php
+++ app/public/wp-content/themes/university-theme/front-page.php
@@ -31,8 +31,18 @@ get_header();
 
             <div class="event-summary">
                 <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Mar</span>
-                    <span class="event-summary__day">25</span>
+                    <span class="event-summary__month">
+                        <?php
+                        // get_field is from the ACF plugin
+                        $eventDate = new DateTime(get_field("event_date"));
+                        echo $eventDate->format('M');
+                        ?>
+                    </span>
+                    <span class="event-summary__day">
+                        <?php
+                        echo $eventDate->format('d');
+                        ?>
+                    </span>
                 </a>
                 <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>">
